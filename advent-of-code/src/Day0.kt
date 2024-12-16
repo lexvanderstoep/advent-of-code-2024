1a51b6da0d34e412142cc8858443da4cba50fd45
Lex van der Stoep <lex.vanderstoep@imc.com>
2024-12-16 20:50:02 +0100

    Add day template

---
+++ advent-of-code/src/Day0.kt
@@ -0,0 +1,14 @@
+import java.io.File
+
+class Day0(inputPath: String) {
+
+    fun computePartOne() = 0
+
+    fun computePartTwo() = 0
+}
+
+fun main() {
+    val day0 = Day0("resources/day_0_sample.txt")
+    println("Part one: ${day0.computePartOne()}")
+    println("Part two: ${day0.computePartTwo()}")
+}
